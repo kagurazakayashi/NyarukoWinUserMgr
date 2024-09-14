42f563e50861ad9a8d3a504baf25c4f5cdf54f04
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-09-14 20:26:33 +0800

    创建空白初始项目

---
+++ UserInfoLoader/UserInfoLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UserInfoType;
+
+namespace UserInfoLoader
+{
+    public class UserInfoLoader
+    {
+        public List<UserInfo> LoadUserData()
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            return users;
+        }
+    }
+}
---
+++ UserInfoType/UserInfoType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserInfoType
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public string AccountExpires { get; set; }
+        public bool Disabled { get; set; }
+        public bool PasswordNeverExpires { get; set; }
+        public bool UserMayNotChangePassword { get; set; }
+        public List<string> Groups { get; set; }
+    }
+}
